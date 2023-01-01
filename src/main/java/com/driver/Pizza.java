3a44894be067eb08f973d92f7d6fb2380431e28b
Lizter <lizterdsouza@gmail.com>
2023-01-01 22:51:56 +0530

    codef

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -3,12 +3,34 @@ package com.driver;
 public class Pizza {
 
     private int price;
-    private Boolean isVeg;
+    private int Base;
+    private Boolean Veg;
     private String bill;
+    private int extraCheese;
+    private int topping;
+    private int paperBag;
+    private boolean cheeseFlag;
+    private boolean toppingFlag;
+    private boolean paperBagFlag;
+    private boolean billGenerated;
 
     public Pizza(Boolean isVeg){
-        this.isVeg = isVeg;
-        // your code goes here
+        this.Veg = isVeg;
+        bill = "";
+        this.extraCheese = 80;
+        this.paperBag = 20;
+
+        if( Veg ){
+            this.Base = 300;
+            this.price = Base;
+            this.topping = 70;
+        }
+        else{
+            this.Base = 400;
+            this.price = Base;
+            this.topping = 120;
+        }
+
     }
 
     public int getPrice(){
@@ -16,19 +38,50 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if( cheeseFlag == false ){
+            cheeseFlag = true;
+            this.price += this.extraCheese;
+        }
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if( Veg ){
+            if( toppingFlag == false ){
+                this.price += this.topping;
+                toppingFlag = true;
+            }
+        }
+        else{
+            if( toppingFlag == false ){
+                toppingFlag = true;
+                this.price += this.topping;
+            }
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if( paperBagFlag == false ){
+            paperBagFlag = true;
+            this.price += paperBag;
+        }
     }
 
     public String getBill(){
-        // your code goes here
+        if( billGenerated == false ){
+            this.bill += "Base Price Of The Pizza: "+this.Base+"\n";
+            if( cheeseFlag ){
+                this.bill += "Extra Cheese Added: "+this.extraCheese+"\n";
+            }
+            if( toppingFlag ){
+                this.bill += "Extra Toppings Added: "+this.topping+"\n";
+            }
+            if( paperBagFlag ){
+                this.bill += "Paperbag Added: "+this.paperBag+"\n";
+            }
+            this.bill += "Total Price: "+this.price+"\n";
+            billGenerated = true;
+        }
+
         return this.bill;
     }
 }
